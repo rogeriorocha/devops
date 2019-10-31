1f1231f3a12ab6ff40bc732a63bd60041e744d6f
Rogério Rocha <rogeriosilvarocha@gmail.com>
2019-10-30 23:31:01 -0300

    pipeline to py-fs

--- 98-install-jenkins/02-pipeline.groovy
+++ 98-install-jenkins/02-pipeline.groovy
@@ -11,31 +11,32 @@ podTemplate(
     def GIT_COMMIT
     def GIT_BRANCH
     node('worker') {
-        def GIT_REPOS = git credentialsId: 'rpsr-github', url: 'git@github.com:rogeriorocha/docker-spring-monitor-log.git'
-        
-        echo GIT_REPOS.toString()
-        
-        GIT_COMMIT = GIT_REPOS.GIT_COMMIT
-        GIT_BRANCH = GIT_REPOS.GIT_BRANCH
         
         echo 'Iniciando Pipeline'
         stage('checkout') {
+            echo 'Clonando repositorio'
             
-            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'rpsr-github', url: 'git@github.com:rogeriorocha/docker-spring-monitor-log.git']]])
+            def GIT_REPOS = git credentialsId: 'rpsr-github', url: 'git@github.com:rogeriorocha/pyFS.git'
+            
+            //echo GIT_REPOS.toString()
+            
+            GIT_COMMIT = GIT_REPOS.GIT_COMMIT
+            GIT_BRANCH = GIT_REPOS.GIT_BRANCH
+            //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'rpsr-github', url: 'git@github.com:rogeriorocha/docker-spring-monitor-log.git']]])
             sh 'ls -ltra'
             
         }
         stage('package') {
+            echo 'package...'
             container('c-docker') {
                 withCredentials([usernamePassword(credentialsId: 'docker-hub-rogeriosilvarocha', passwordVariable: 'DH_PASSWORD', usernameVariable: 'DH_USER')]) {
                     
                     sh """
                     docker login -u ${DH_USER} -p ${DH_PASSWORD}
-                    docker build -t rogeriosilvarocha/my-fs:0.1 .
-                    docker push rogeriosilvarocha/my-fs:0.1
+                    docker build -t rogeriosilvarocha/py-fs:0.1 .
+                    docker push rogeriosilvarocha/py-fs:0.1
                     docker ps
                     docker image ls
-                        
                     """    
                 }                
 
